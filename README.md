49d288549e1e0876837178e7ddd1a1a8c40e7a59
Dan Hounshell <dan@danhounshell.com>
2018-01-26 13:59:30 -0500

    Updated sql

--- README.md
+++
@@ -1,2 +0,0 @@
-# gatsby-source-graffiti-sqlserver
-Gatsby plugin which pulls blog posts out of a Graffiti SQL Server database
--- src/sql.js
+++ src/sql.js
@@ -3,7 +3,7 @@ const sql = require( "seriate" );
 let log;
 
 const getNewPosts = ( queryOptions, lastFetched ) => {
-    let query = `SELECT p.Title as title, p.PostBody as postBody, p.CreatedOn as createdOn, p.ModifiedOn as modifiedOn, p.Published as publishedOn, p.Content_Type as contentType, p.Name as name, p.Tag_List as tagList, c.Name as categoryName, p.ModifiedBy as modifiedBy, p.CreatedBy as createdBy, p.UniqueId as id FROM graffiti_posts p INNER JOIN graffiti_Categories c ON p.CategoryId = c.Id WHERE p.IsPublished=1 AND p.IsDeleted=0 AND c.IsDeleted=0 AND p.CategoryId = ${ queryOptions.categoryId }`;
+    let query = `SELECT p.Title as title, p.PostBody as postBody, p.CreatedOn as createdOn, p.ModifiedOn as modifiedOn, p.Published as publishedOn, p.Content_Type as contentType, p.Name as slug, p.Tag_List as tagList, c.Name as categoryName, p.ModifiedBy as modifiedBy, p.CreatedBy as createdBy, p.UniqueId as id FROM graffiti_posts p INNER JOIN graffiti_Categories c ON p.CategoryId = c.Id WHERE p.IsPublished=1 AND p.IsDeleted=0 AND c.IsDeleted=0 AND p.CategoryId = ${ queryOptions.categoryId }`;
     if ( lastFetched ) {
         query+= ` AND p.ModifiedOn > '${ new Date( lastFetched ).toISOString() }'`;
     }
@@ -14,7 +14,7 @@ const getNewPosts = ( queryOptions, lastFetched ) => {
 
 const getDeletedPosts = ( queryOptions, lastFetched ) => {
     if ( lastFetched ) {
-        let query = `SELECT p.Title as title, p.PostBody as postBody, p.CreatedOn as createdOn, p.ModifiedOn as modifiedOn, p.Published as publishedOn, p.Content_Type as contentType, p.Name as name, p.Tag_List as tagList, c.Name as categoryName, p.ModifiedBy as modifiedBy, p.CreatedBy as createdBy, p.UniqueId as id FROM graffiti_posts p INNER JOIN graffiti_Categories c ON p.CategoryId = c.Id WHERE ( p.IsPublished=0 OR p.IsDeleted=1 OR c.IsDeleted=1 ) AND p.CategoryId = ${ queryOptions.categoryId } AND p.ModifiedOn > '${ new Date( lastFetched ).toISOString() }' ORDER BY CreatedOn DESC;`;    
+        let query = `SELECT p.Title as title, p.PostBody as postBody, p.CreatedOn as createdOn, p.ModifiedOn as modifiedOn, p.Published as publishedOn, p.Content_Type as contentType, p.Name as slug, p.Tag_List as tagList, c.Name as categoryName, p.ModifiedBy as modifiedBy, p.CreatedBy as createdBy, p.UniqueId as id FROM graffiti_posts p INNER JOIN graffiti_Categories c ON p.CategoryId = c.Id WHERE ( p.IsPublished=0 OR p.IsDeleted=1 OR c.IsDeleted=1 ) AND p.CategoryId = ${ queryOptions.categoryId } AND p.ModifiedOn > '${ new Date( lastFetched ).toISOString() }' ORDER BY CreatedOn DESC;`;
         return executeQuery( query );
     } else {
         // this has never been run before so there should be no existing nodes to delete
@@ -23,7 +23,7 @@ const getDeletedPosts = ( queryOptions, lastFetched ) => {
 };
 
 const executeQuery = ( query ) => {
-    return sql.execute( { 
+    return sql.execute( {
         query: query
     } ).then( results => {
         return results;
@@ -33,11 +33,11 @@ const executeQuery = ( query ) => {
     } );
 };
 
-module.exports = function( config, logger ) { 
+module.exports = function( config, logger ) {
     sql.setDefaultConfig( config );
     log = logger;
     return {
-        getNewPosts, 
+        getNewPosts,
         getDeletedPosts
     };
 };
