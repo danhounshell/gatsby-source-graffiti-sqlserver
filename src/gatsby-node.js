eda8107c528b3180dbaf6f98967f67495572c7f7
Dan Hounshell <dan@danhounshell.com>
2018-02-16 09:27:07 -0500

    removed creation of ids array from plugin instead do it in onNodeCreated handler if desired

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -14,7 +14,7 @@ const writeNodeToJson = ( post, options ) => {
 		const filePath = path.join( options.exportToJson.path, `/${ fileTitle }` );
 		const fileAlreadyExists = fs.existsSync( filePath );
 		if ( options.exportToJson.overwriteExisting || !fileAlreadyExists ) {
-			fs.writeFileSync( filePath, JSON.stringify( post, null, 4 ), "utf8" )
+			fs.writeFileSync( filePath, JSON.stringify( post, null, 4 ), "utf8" );
 		}
 	}
 };
@@ -22,31 +22,31 @@ const writeNodeToJson = ( post, options ) => {
 // remove json file
 
 const transformPostToNode = ( post, options ) => {
-    const internal = {
-        type: "GraffitiBlogPost",
-        mediaType: post.contentType,
-        contentDigest: crypto.createHash( "md5" ).update( JSON.stringify( post ) ).digest( "hex" )
-    }
+	const internal = {
+		type: "GraffitiBlogPost",
+		mediaType: post.contentType,
+		contentDigest: crypto.createHash( "md5" ).update( JSON.stringify( post ) ).digest( "hex" )
+	}
 
-    if ( post.comments && post.comments.length > 0 ) {
-    	const oldComments = _.cloneDeep( post.comments );
-    	post.comments = [];
-    	_.each( oldComments, ( oldComment ) => {
-    		const newComment = {
-    			id: oldComment.id,
-    			date: oldComment.publishedOn,
-    			author: {
-    				name: oldComment.author,
-    				url: oldComment.authorUrl
-    			},
-    			html: oldComment.content,
-    			isTrackback: oldComment.isTrackback
-    		}
-    		post.comments.push( newComment );
-    	} );
-    }
+	if ( post.comments && post.comments.length > 0 ) {
+		const oldComments = _.cloneDeep( post.comments );
+		post.comments = [];
+		_.each( oldComments, ( oldComment ) => {
+			const newComment = {
+				id: oldComment.id,
+				date: oldComment.publishedOn,
+				author: {
+					name: oldComment.author,
+					url: oldComment.authorUrl
+				},
+				html: oldComment.content,
+				isTrackback: oldComment.isTrackback
+			}
+			post.comments.push( newComment );
+		} );
+	}
 
-    let newPostBody = post.postBody;
+	let newPostBody = post.postBody;
 	if ( options.replaceStrings ) {
 		_.each( options.replaceStrings, ( replaceString ) => {
 			const regex = new RegExp( replaceString.source, "g" );
@@ -54,7 +54,7 @@ const transformPostToNode = ( post, options ) => {
 		} );
 	}
 
-    let cover = "";
+	let cover = "";
 	const regex = /<img[^>]+src="?([^"\s]+)"?[^>]*\/>/g;
 	const regexResults = regex.exec( newPostBody );
 	if ( regexResults ) {
@@ -62,41 +62,40 @@ const transformPostToNode = ( post, options ) => {
 	}
 
 	const node = {
-        id: post.id,
-    	ids: [ post.id, "" ],
-    	internal,
-        parent: null,
-        children: [],
-        html: newPostBody,
-        slug: _.kebabCase( post.slug ),
-        author: { name: post.createdBy },
-        comments: post.comments,
-    	frontmatter: {
-    		title: post.title,
-            date: post.publishedOn,
-            description: _.truncate(
-            	htmlToText.fromString(
-            		newPostBody,
-            		{
-            			ignoreHref: true,
-            			ignoreImage: true
-            		}
-            	),
-            	{
-            		length: options.descriptionLength,
-            		separator: " "
-            	}
-            ),
+		id: post.id,
+		internal,
+		parent: null,
+		children: [],
+		html: newPostBody,
+		slug: _.kebabCase( post.slug ),
+		author: { name: post.createdBy },
+		comments: post.comments,
+		frontmatter: {
+			title: post.title,
+			date: post.publishedOn,
+			description: _.truncate(
+				htmlToText.fromString(
+					newPostBody,
+					{
+						ignoreHref: true,
+						ignoreImage: true
+					}
+				),
+				{
+					length: options.descriptionLength,
+					separator: " "
+				}
+			),
 			tags: post.tags.split(","),
-            layout: ( post.category.trim().toLowerCase() === "uncategorized" ) ? "page" : "post",
-            category: post.category,
-            cover: cover,
-            draft: false
-    	}
-    }
+			layout: ( post.category.trim().toLowerCase() === "uncategorized" ) ? "page" : "post",
+			category: post.category,
+			cover: cover,
+			draft: false
+		}
+	}
 
-    // Stringify date objects
-    return JSON.parse( JSON.stringify( node ) );
+	// Stringify date objects
+	return JSON.parse( JSON.stringify( node ) );
 }
 
 exports.sourceNodes = async ( { boundActionCreators, reporter, store }, pluginOptions ) => {
@@ -121,57 +120,57 @@ exports.sourceNodes = async ( { boundActionCreators, reporter, store }, pluginOp
 			includeComments: false
 		}
 	}
-    const { createNode, deleteNode, setPluginStatus } = boundActionCreators;
-    const options = _.merge( defaultOptions, pluginOptions );
+	const { createNode, deleteNode, setPluginStatus } = boundActionCreators;
+	const options = _.merge( defaultOptions, pluginOptions );
 
-    const dataSource = ( options.dataSource.type === "static" ) ?
-    	null :
-    	( options.dataSource.type === "sql" ) ? SQL( options.dataSource, reporter ) :
-    	json( options.dataSource, reporter );
+	const dataSource = ( options.dataSource.type === "static" ) ?
+		null :
+		( options.dataSource.type === "sql" ) ? SQL( options.dataSource, reporter ) :
+		json( options.dataSource, reporter );
 
-    let lastFetchedPosts, lastDeletedPosts;
-    if ( store && store.getState() && store.getState().status && store.getState().status.plugins &&
-        store.getState().status.plugins[`gatsby-source-graffiti-sqlserver`] )
-    {
-        const thisPlugin = store.getState().status.plugins[`gatsby-source-graffiti-sqlserver`];
-        if ( thisPlugin.status ) {
-            lastFetchedPosts = thisPlugin.status.lastFetchedPosts;
-            lastDeletedPosts = thisPlugin.status.lastDeletedPosts;
-        }
-    }
+	let lastFetchedPosts, lastDeletedPosts;
+	if ( store && store.getState() && store.getState().status && store.getState().status.plugins &&
+		store.getState().status.plugins[`gatsby-source-graffiti-sqlserver`] )
+	{
+		const thisPlugin = store.getState().status.plugins[`gatsby-source-graffiti-sqlserver`];
+		if ( thisPlugin.status ) {
+			lastFetchedPosts = thisPlugin.status.lastFetchedPosts;
+			lastDeletedPosts = thisPlugin.status.lastDeletedPosts;
+		}
+	}
 
-    // get only posts modified since last check
-    let newPosts = ( dataSource ) ?
-    	await dataSource.getNewPosts( options, lastFetchedPosts ) :
-    	staticData;
-    if ( newPosts && newPosts.length > 0 ) {
-        newPosts.forEach( post => {
+	// get only posts modified since last check
+	let newPosts = ( dataSource ) ?
+		await dataSource.getNewPosts( options, lastFetchedPosts ) :
+		staticData;
+	if ( newPosts && newPosts.length > 0 ) {
+		newPosts.forEach( post => {
 			writeNodeToJson( post, options );
-            createNode( transformPostToNode( post, options ) );
-        } );
-        reporter.info( `fetched ${ newPosts.length } new nodes`)
-        setPluginStatus( { lastFetchedPosts: Date.now() } );
-    } else {
-        reporter.info( "fetched 0 new nodes" );
-    }
+			createNode( transformPostToNode( post, options ) );
+		} );
+		reporter.info( `fetched ${ newPosts.length } new nodes`)
+		setPluginStatus( { lastFetchedPosts: Date.now() } );
+	} else {
+		reporter.info( "fetched 0 new nodes" );
+	}
 
-    // delete any nodes where post has been deleted since last check
-    if ( lastDeletedPosts ) {
-        let deletedPosts = ( dataSource ) ?
-       		await dataSource.getDeletedPosts( options, lastDeletedPosts ) :
-       		null;
-        if ( deletedPosts && deletedPosts.length > 0 ) {
-            deletedPosts.forEach( post => {
-                let node = transformPostToNode( post, options );
-                deleteNode( node.id, node );
-            } );
-            reporter.info( `deleted ${ deletedPosts.length } nodes`)
-            setPluginStatus( { lastDeletedPosts: Date.now() } );
-        } else {
-            reporter.info( "deleted 0 nodes" );
-        }
-    } else {
-        // this has never been run before so there should be no existing nodes to delete
-        reporter.info( "deleted 0 nodes" );
-    }
+	// delete any nodes where post has been deleted since last check
+	if ( lastDeletedPosts ) {
+		let deletedPosts = ( dataSource ) ?
+			await dataSource.getDeletedPosts( options, lastDeletedPosts ) :
+			null;
+		if ( deletedPosts && deletedPosts.length > 0 ) {
+			deletedPosts.forEach( post => {
+				let node = transformPostToNode( post, options );
+				deleteNode( node.id, node );
+			} );
+			reporter.info( `deleted ${ deletedPosts.length } nodes`)
+			setPluginStatus( { lastDeletedPosts: Date.now() } );
+		} else {
+			reporter.info( "deleted 0 nodes" );
+		}
+	} else {
+		// this has never been run before so there should be no existing nodes to delete
+		reporter.info( "deleted 0 nodes" );
+	}
 };
